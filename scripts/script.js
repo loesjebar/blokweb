1baba62767f048481cd3b3e13a84574c21e8521c
loesjebar <153863385+loesjebar@users.noreply.github.com>
2024-12-05 10:51:57 +0100

    hele hamburger menu
    nice

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
-// JavaScript Document
-console.log("hi");
+const btnMenu = document.querySelector('header nav button')
+const menuNav = document.querySelector('body > section')
+const closeBtn = document.querySelector('body > section article button')
+
+btnMenu.addEventListener('click', ()=>{
+    menuNav.classList.add('active')
+})
+
+closeBtn.addEventListener('click', ()=>{
+
+    menuNav.classList.remove('active')
+})
